613d38c309c56b25e5421c6b1a29284fb9a4b560
iamvinitk <kananivinit97@gmail.com>
2018-11-29 13:23:44 +0530

    Added Prgm 7

--- prgm7/prgm7.py
+++ prgm7/prgm7.py
@@ -7,48 +7,59 @@ import pandas as pd
 
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.models import BayesianModel
+from pgmpy.inference import VariableElimination
 
-from pgmpy.inference import VariableElimination  # Read the attributes
-
-raw_data = pd.read_csv('dataset.csv')
+dataset = pd.read_csv('dataset.csv')
 print("Input Data and Shape")
-print(raw_data.shape)
-data = pd.DataFrame(raw_data, columns=['A', 'G', 'CP', 'BP', 'CH', 'ECG', 'HR', 'EIA', 'HD'])
-data_train = data[: int(data.shape[0] * 0.80)]
-variable_map = {
-    "A": ["< 45", "45--55", "\geq 55"],
-    "G": ["Female", "Male"],
-    "CP": ["Typical", "Atypical", "Non-Anginal", "None"],
-    "BP": ["Low", "High"],
-    "CH": ["Low", "High"],
-    "ECG": ["Normal", "Abnormal"],
-    "HR": ["Low", "High"],
-    "EIA": ["No", "Yes"],
-    "HD": ["No", "Yes"]
-}
-# creating the baysian network
-model = BayesianModel([('HD', 'CP'), ('G', 'BP'), ('A', 'CH'), ('G', 'CH'), ('HD', 'ECG'), ('HD',
-                                                                                            'HR'), ('BP', 'HR'),
-                       ('A', 'HR'), ('BP', 'HD'), ('CH', 'HD'), ('HD', 'EIA')])
+print(dataset)
+model = BayesianModel(
+    [('HD', 'AGE'), ('HD', 'GENDER'), ('CP', 'AGE'), ('CHOLESTEROL', 'AGE'), ('HD', 'BP'), ('GENDER', 'CP')])
 
-# Learning CPDs using Maximum Likelihood Estimators
-print('\nLearning CPDs using Maximum Likelihood Estimators...')
-model.fit(data, estimator=MaximumLikelihoodEstimator)
+print(model.nodes())
+print(model.edges())
 
-# Inferencing with Bayesian Network
+"""
+
+         CHOLESTEROL
+              \
+               \
+                \
+                 \
+                  \
+                   \        HD-----
+                    \      / \     \
+                     \    /   \     \
+                      \  /     \     \
+                      AGE   GENDER  BP
+                       ^     /    
+                       |    /
+                       |   /                  
+                       |  /
+                       | /
+                       CP
+"""
+
+X_train = dataset.loc[:200, :]  # Split dataset
+X_test = dataset.loc[201:, :]
+
+model.fit(dataset, estimator=MaximumLikelihoodEstimator)
+
 print('\n Inferencing with Bayesian Network:')
+
 HeartDisease_infer = VariableElimination(model)
 for cpd in model.get_cpds():
-    print("CPD of {variable}:".format(variable=cpd.variable))
-    print(cpd)
+    # print("CPD of {variable}:".format(variable=cpd.variable))
+    # print(cpd)
     print(model.check_model())
-# Computing the probability of HD given as a Variable.
-print('\n1.Probability of HeartDisease given Gender=Female')
-
-q = HeartDisease_infer.query(variables=['HD'], evidence={'G': 1})
+print('\n1.Probability of HeartDisease given Gender = Female')
+q = HeartDisease_infer.query(variables=['HD'], evidence={'GENDER': 1})
 print(q['HD'])
 
-print('\n2. Probability of HeartDisease given BP= Low')
-
+print('\n2. Probability of HeartDisease given BP = Low')
 q = HeartDisease_infer.query(variables=['HD'], evidence={'BP': 1})
 print(q['HD'])
+
+print(model.get_independencies())
+
+print(model.active_trail_nodes('HD'))
+
